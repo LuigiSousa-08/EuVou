f82fa6007af7cf4276852552bf16cb3e116c6f6a
LuigiSousaa <luigienricossilva@gmail.com>
2024-02-29 16:54:02 -0300

    first commit

---
+++ app/src/main/java/com/example/euvou/Splash_Screen.kt
@@ -0,0 +1,22 @@
+package com.example.euvou
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+
+class Splash_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+        }, 5000)
+    }
+}
